6eb1f36fc0a6e233d0fce9b0d2d62dba28a812c9
MariaCaroll <carolfavola@gmail.com>
2020-06-26 20:50:03 -0300

    Recuperar senha com erro ao abrir página

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -2,34 +2,61 @@
 
 namespace Hcode;
 
+use Hcode\DB\Sql;
+
 class Model
  {
     private $values = [];
 
-    public function __call($name, $args)
-    {
-        $method = substr($name, 0, 3);
-        $fieldName = substr($name, 3, strlen($name));
+	public function __call($name, $args)
+	{
 
-        switch ($method)
-        {
-            case "get":
-                $this->values[$fieldName];
-            break;
-            case "set":
-                $this->values[$fieldName] = $args[0];
-            break;        
-        }
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method)
+		{
+
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+			break;
+
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
+
+		}
+
+	}
+
+	public function setData($data = array())
+	{
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value);
+
+		}
+
+	}
+
+	public function getValues()
+	{
+
+		return $this->values;
+
     }
-    public function setData($data = array())
+    
+    public function get($iduser)
     {
-        foreach ($data as $key => $value){
-            $this->{"set".$key}($value);
-        }
-    }
-    public function getValues() 
-    {
-        return $this->values;
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser
+        ", array(
+            ":iduser=>$iduser"
+            ));
+
+            $this->setData($results[0]);
     }
 
     
