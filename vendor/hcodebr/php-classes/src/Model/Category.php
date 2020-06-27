39977d4aeedbb2573b711d88dde4ae2495bf08ce
MariaCaroll <carolfavola@gmail.com>
2020-06-26 23:31:04 -0300

    listar Categorias com erro

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -29,7 +29,7 @@ class Category extends Model
 
 		$this->setData($results[0]);
 
-		//Category::updateFile();
+		Category::updateFile();
 
 	}
 
@@ -59,6 +59,20 @@ class Category extends Model
 
 	}
 
+	public static function updateFile()
+	{
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
+	}
    
 }
 ?>
---
+++ views-cache/categories-menu.c638fa5529e37e1a0a6b20d4c2029b41.rtpl.php
@@ -0,0 +1,2 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/1">Salon Line Cachos</a></li>
+<li><a href="/categories/2">Salon Line Lisos</a></li>
